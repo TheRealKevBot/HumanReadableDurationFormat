d948cca11a457c9100271de9707028a62aeed2ae
TheRealKevBot <Kglas001@gmail.com>
2021-08-13 12:34:52 -0600

    First

---
+++ index.js
@@ -0,0 +1,18 @@
+//  Write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+//  The function must accept a non-negative integer. 
+// If it is zero, it just returns "now". 
+// Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+
+
+function formatDuration (seconds) {
+    // Complete this function
+} 
+
+console.log(formatDuration(1), "1 second");
+console.log(formatDuration(62), "1 minute and 2 seconds");
+console.log(formatDuration(120), "2 minutes");
+console.log(formatDuration(3600), "1 hour");
+console.log(formatDuration(3662), "1 hour, 1 minute and 2 seconds");
+  
