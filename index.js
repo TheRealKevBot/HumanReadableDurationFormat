73188c406790e74a3efde1fab93e3ae49489cb75
TheRealKevBot <Kglas001@gmail.com>
2021-08-13 15:18:37 -0600

    Build1

--- index.js
+++ index.js
@@ -19,26 +19,30 @@ function formatDuration (seconds) {
         remainder = remainder%timeList.year
     }
     if (remainder >= timeList.day && remainder < timeList.year) {
+        remainder%timeList.day === 0 && time != '' ? time = time.slice(0, -1).concat(' and') : null
         Math.floor(remainder/timeList.day) === 1 ? time = time.concat(' 1 day,') : time = time.concat(` ${Math.floor(remainder/timeList.day)} days,`) 
         remainder = remainder%timeList.day
     }
     if (remainder >= timeList.hour && remainder < timeList.day) {
+        remainder%timeList.hour === 0 && time != '' ? time = time.slice(0, -1).concat(' and') : null
         Math.floor(remainder/timeList.hour) === 1 ? time = time.concat(' 1 hour,') : time = time.concat(` ${Math.floor(remainder/timeList.hour)} hours,`) 
         remainder = remainder%timeList.hour
     }
     if (remainder >= timeList.minute && remainder < timeList.hour) {
-        Math.floor(remainder/timeList.minute) === 1 ? time = time.concat(' 1 minute') : time = time.concat(` ${Math.floor(remainder/timeList.minute)} minutes`) 
+        remainder%timeList.minute === 0 && time != '' ? time = time.slice(0, -1).concat(' and') : null
+        Math.floor(remainder/timeList.minute) === 1 ? time = time.concat(' 1 minute,') : time = time.concat(` ${Math.floor(remainder/timeList.minute)} minutes,`) 
         remainder = remainder%timeList.minute
     }
     if (remainder <= 59) {
-        seconds === 0 ? time = time.concat('Now') 
-        : remainder === 1 ? time = time.concat(' and 1 second') 
-        : remainder > 1 ? time = time.concat(` and ${remainder} seconds`) : null
+        remainder > 0 && time != '' ? time = time.slice(0, -1).concat(' and') : null
+        seconds === 0 ? time = 'now' 
+        : remainder === 1 ? time = time.concat(' 1 second') 
+        : remainder > 1 ? time = time.concat(` ${remainder} seconds`) : null
     }
     return time.charAt(time.length - 1) === ',' ? time = time.slice(0, -1).trim() : time.trim()
 } 
 
-console.log(formatDuration(0), "Now");
+console.log(formatDuration(0), "now");
 console.log(formatDuration(1), "1 second");
 console.log(formatDuration(62), "1 minute and 2 seconds");
 console.log(formatDuration(120), "2 minutes");
@@ -53,4 +57,6 @@ console.log(formatDuration(270183), "3 days, 3 hours, 3 minutes and 3 seconds");
 console.log(formatDuration(31536000 ), "1 year");
 console.log(formatDuration(63072000 ), "2 years");
 console.log(formatDuration(63252122), "2 years, 2 days, 2 hours, 2 minutes and 2 seconds");
+
+console.log(formatDuration(132030240), "4 years, 68 days, 3 hours and 4 minutes");
 
